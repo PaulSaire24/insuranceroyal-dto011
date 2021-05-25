10535d42ae96c572e342430047fe9ea297c50c55
AUGUSTO EDUARDO MARTINEZ SALAZAR <P029820@PE.igrupobbva>
2021-05-25 12:44:47 -0500

    name is new field created in DocumentTypeDTO

--- src/main/java/com/bbva/pisd/dto/insurance/commons/DocumentTypeDTO.java
+++ src/main/java/com/bbva/pisd/dto/insurance/commons/DocumentTypeDTO.java
@@ -2,16 +2,22 @@ package com.bbva.pisd.dto.insurance.commons;
 
 public class DocumentTypeDTO extends CommonOutputFieldsDTO {
 
+    private String name;
+
 	public DocumentTypeDTO(String id) {
 	    this.setId(id);
 
 	}
 	public DocumentTypeDTO() { }
 
+    public String getName() { return name; }
+    public void setName(String name) { this.name = name; }
+
     @Override
     public String toString() {
         final StringBuilder sb = new StringBuilder("DocumentTypeDTO{");
         sb.append("id='").append(this.getId()).append('\'');
+        sb.append(", name='").append(name).append('\'');
         sb.append(", description='").append(this.getDescription()).append('\'');
         sb.append('}');
         return sb.toString();
