f5b6d617d1206ddcc293cf53edfe3671324a7a73
paul.rivera <paul.rivera@bbva.com>
2021-06-10 19:56:48 -0500

    Modify installmentPlans for installmentsPlans

--- src/main/java/com/bbva/pisd/dto/insurance/financing/FinancingPlanDTO.java
+++ src/main/java/com/bbva/pisd/dto/insurance/financing/FinancingPlanDTO.java
@@ -8,6 +8,10 @@ import java.util.List;
 public class FinancingPlanDTO extends CommonOutputFieldsDTO {
 
     private List<InstallmentsDTO> installmentsPlans;
+    private List<InstallmentsDTO> installmentPlans;
+
+    public List<InstallmentsDTO> getInstallmentPlans() { return installmentPlans; }
+    public void setInstallmentPlans(List<InstallmentsDTO> installmentPlans) { this.installmentPlans = installmentPlans; }
 
     public List<InstallmentsDTO> getInstallmentsPlans() { return installmentsPlans; }
     public void setInstallmentsPlans(List<InstallmentsDTO> installmentsPlans) { this.installmentsPlans = installmentsPlans; }
@@ -17,7 +21,8 @@ public class FinancingPlanDTO extends CommonOutputFieldsDTO {
         sb.append("quotationId='").append(this.getQuotationId()).append('\'');
         sb.append("startDate='").append(this.getStartDate()).append('\'');
         sb.append("maturityDate='").append(this.getMaturityDate()).append('\'');
-        sb.append("installmentPlans='").append(getInstallmentsPlans()).append('\'');
+        sb.append("installmentsPlans='").append(getInstallmentsPlans()).append('\'');
+        sb.append("installmentPlans='").append(getInstallmentPlans()).append('\'');
         sb.append('}');
         return sb.toString();
     }
