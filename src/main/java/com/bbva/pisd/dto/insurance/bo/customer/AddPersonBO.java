03e94ee3ec442617d4b50cd897aef90249bb465d
AUGUSTO EDUARDO MARTINEZ SALAZAR <P029820@PE.igrupobbva>
2021-07-01 10:21:10 -0500

    creating new field

---
+++ src/main/java/com/bbva/pisd/dto/insurance/bo/customer/AddPersonBO.java
@@ -0,0 +1,9 @@
+package com.bbva.pisd.dto.insurance.bo.customer;
+
+public class AddPersonBO {
+
+    public DataPersona payload;
+
+    public DataPersona getPayload() { return payload; }
+    public void setPayload(DataPersona payload) { this.payload = payload; }
+}
