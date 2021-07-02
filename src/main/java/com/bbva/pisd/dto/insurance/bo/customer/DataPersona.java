8b63d8b838e4c7d7b2a53df1eef7c9166ca21a1e
AUGUSTO EDUARDO MARTINEZ SALAZAR <P029820@PE.igrupobbva>
2021-07-02 04:00:04 -0500

    test

--- src/main/java/com/bbva/pisd/dto/insurance/bo/customer/DataPersona.java
+++ src/main/java/com/bbva/pisd/dto/insurance/bo/customer/DataPersona.java
@@ -1,9 +1,11 @@
 package com.bbva.pisd.dto.insurance.bo.customer;
 
 import com.bbva.pisd.dto.insurance.bo.CommonOutputFieldsBO;
+import org.codehaus.jackson.annotate.JsonIgnoreProperties;
 
 import java.util.List;
 
+@JsonIgnoreProperties(ignoreUnknown = true)
 public class DataPersona extends CommonOutputFieldsBO {
 
     private List<Persona> persona;
