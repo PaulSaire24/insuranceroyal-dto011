7f379d390a1294764aa404e1e125f77a71be9149
AUGUSTO EDUARDO MARTINEZ SALAZAR <P029820@PE.igrupobbva>
2021-08-02 12:05:33 -0500

    adding new fields in the request and response

--- src/main/java/com/bbva/pisd/dto/insurance/search/InsuranceSearchDTO.java
+++ src/main/java/com/bbva/pisd/dto/insurance/search/InsuranceSearchDTO.java
@@ -1,18 +1,20 @@
 package com.bbva.pisd.dto.insurance.search;
 
 import com.bbva.apx.dto.AbstractDTO;
+import com.bbva.pisd.dto.insurance.commons.CommonOutputFieldsDTO;
 import com.bbva.pisd.dto.insurance.commons.IdentityDocumentDTO;
+import com.bbva.pisd.dto.insurance.simulation.VehicleDTO;
 
-public class InsuranceSearchDTO extends AbstractDTO {
+public class InsuranceSearchDTO extends CommonOutputFieldsDTO {
 
     private IdentityDocumentDTO document;
-    private String licensePlate;
+    private VehicleDTO vehicle;
     private String traceId;
 
     public IdentityDocumentDTO getDocument() { return document; }
     public void setDocument(IdentityDocumentDTO document) { this.document = document; }
-    public String getLicensePlate() { return licensePlate; }
-    public void setLicensePlate(String licensePlate) { this.licensePlate = licensePlate; }
+    public VehicleDTO getVehicle() { return vehicle; }
+    public void setVehicle(VehicleDTO vehicle) { this.vehicle = vehicle; }
     public String getTraceId() { return traceId; }
     public void setTraceId(String traceId) { this.traceId = traceId; }
 
@@ -20,8 +22,9 @@ public class InsuranceSearchDTO extends AbstractDTO {
     public String toString() {
         final StringBuilder sb = new StringBuilder("InsuranceSearchDTO{");
         sb.append("document=").append(document);
-        sb.append(", licensePlate='").append(licensePlate).append('\'');
+        sb.append(", vehicle='").append(vehicle).append('\'');
         sb.append(", traceId='").append(traceId).append('\'');
+        sb.append(", product='").append(this.getProduct()).append('\'');
         sb.append('}');
         return sb.toString();
     }
