b554686cdd68584bf9dda80b6265bf394c4cbee2
AUGUSTO EDUARDO MARTINEZ SALAZAR <P029820@PE.igrupobbva>
2021-08-10 02:24:45 -0500

    new fields listado cotizacion

--- src/main/java/com/bbva/pisd/dto/insurance/bo/PlanSearchBO.java
+++ src/main/java/com/bbva/pisd/dto/insurance/bo/PlanSearchBO.java
@@ -4,6 +4,8 @@ public class PlanSearchBO extends CommonBO {
 
     private Integer plan;
     private String dscPlan;
+    private String ideProd;
+    private String dscProd;
 
     public Integer getPlan() {
         return plan;
@@ -21,11 +23,21 @@ public class PlanSearchBO extends CommonBO {
         this.dscPlan = dscPlan;
     }
 
+    public String getIdeProd() { return ideProd; }
+
+    public void setIdeProd(String ideProd) { this.ideProd = ideProd; }
+
+    public String getDscProd() { return dscProd; }
+
+    public void setDscProd(String dscProd) { this.dscProd = dscProd; }
+
     @Override
     public String toString() {
         final StringBuilder sb = new StringBuilder("PlanSearchBO{");
         sb.append("plan=").append(plan);
         sb.append(", dscPlan='").append(dscPlan).append('\'');
+        sb.append(", ideProd='").append(ideProd).append('\'');
+        sb.append(", dscProd='").append(dscProd).append('\'');
         sb.append("primaBruta=").append(this.getPrimaBruta());
         sb.append(", primaNeta=").append(this.getPrimaNeta());
         sb.append(", moneda='").append(this.getMoneda()).append('\'');
