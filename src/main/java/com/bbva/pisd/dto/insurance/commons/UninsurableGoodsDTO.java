daf7db34accaa9f7d2052b5d6cda295a0c570872
PC-JUAN <PC@DESKTOP-GDVVMHK>
2021-08-16 17:43:37 -0500

    update DTO search quotation detail

---
+++ src/main/java/com/bbva/pisd/dto/insurance/commons/UninsurableGoodsDTO.java
@@ -0,0 +1,16 @@
+package com.bbva.pisd.dto.insurance.commons;
+
+public class UninsurableGoodsDTO extends CommonDTO{
+
+    @Override
+    public String toString() {
+        final StringBuilder sb = new StringBuilder("UninsurableGoodsDTO{");
+        sb.append("id='").append(this.getId()).append('\'');
+        sb.append(", name='").append(this.getName()).append('\'');
+        sb.append(", isPrincipal='").append(this.getIsPrincipal()).append('\'');
+        sb.append('}');
+        return sb.toString();
+    }
+
+
+}
