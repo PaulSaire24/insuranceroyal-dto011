46dd427a0fb9a93f8c4cb96eb501574cc9b5f0aa
Jose616dop <jose9596979899@gmail.com>
2021-08-23 18:39:23 -0500

    dto's correction for quotation detail 3

--- src/main/java/com/bbva/pisd/dto/insurance/search/SearchQuotationDTO.java
+++ src/main/java/com/bbva/pisd/dto/insurance/search/SearchQuotationDTO.java
@@ -16,12 +16,13 @@ public class SearchQuotationDTO extends CommonOutputFieldsDTO {
         sb.append("holder=").append(this.getHolder());
         sb.append(", licensePlate='").append(this.getLicensePlate()).append('\'');
         sb.append(", id='").append(this.getId()).append('\'');
-        sb.append(", operationDate=").append(this.getOperationDate()).append('\'');;
-        sb.append(", maturityDate=").append(this.getMaturityDate()).append('\'');;
-        sb.append(", product=").append(this.getProduct()).append('\'');;
-        sb.append(", status=").append(this.getStatus()).append('\'');;
-        sb.append(", model=").append(this.getModel()).append('\'');;
-        sb.append(", vehicle=").append(vehicle).append('\'');;
+        sb.append(", operationDate=").append(this.getOperationDate()).append('\'');
+        sb.append(", maturityDate=").append(this.getMaturityDate()).append('\'');
+        sb.append(", product=").append(this.getProduct()).append('\'');
+        sb.append(", status=").append(this.getStatus()).append('\'');
+        sb.append(", model=").append(this.getModel()).append('\'');
+        sb.append(", vehicle=").append(vehicle).append('\'');
+        sb.append(", businessAgent=").append(this.getBusinessAgent()).append('\'');
         sb.append('}');
         return sb.toString();
     }
