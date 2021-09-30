68b0b4253a55e469eaef100f81c45873c1e843bc
RONALD FELIPE DOLORES GUARDIA <P030557@PE.igrupobbva>
2021-09-30 10:55:41 -0500

    Adding some errors for simulation service

--- src/main/java/com/bbva/pisd/dto/insurance/dao/InsuranceProductModalityDAO.java
+++ src/main/java/com/bbva/pisd/dto/insurance/dao/InsuranceProductModalityDAO.java
@@ -7,25 +7,36 @@ public class InsuranceProductModalityDAO extends CommonsDAO {
     public InsuranceProductModalityDAO() {
     }
 
-    public InsuranceProductModalityDAO(String insuranceCompanyModalityId, String insuranceModalityName, String suggestedModalityIndType, BigDecimal insuranceProductId, String insuranceProductDescription) {
+    public InsuranceProductModalityDAO(String insuranceCompanyModalityId, String insuranceModalityName, String suggestedModalityIndType) {
         this.insuranceCompanyModalityId = insuranceCompanyModalityId;
         this.insuranceModalityName = insuranceModalityName;
         this.suggestedModalityIndType = suggestedModalityIndType;
+    }
+
+    public InsuranceProductModalityDAO(String insuranceCompanyModalityId, String insuranceModalityName, String suggestedModalityIndType, BigDecimal insuranceProductId, String insuranceProductDescription) {
+        this(insuranceCompanyModalityId, insuranceModalityName, suggestedModalityIndType);
         this.setInsuranceProductId(insuranceProductId);
         this.setInsuranceProductDescription(insuranceProductDescription);
     }
 
     public InsuranceProductModalityDAO(String insuranceCompanyModalityId, String insuranceModalityName, String insuranceModalityType, String suggestedModalityIndType, BigDecimal publicationOrderNumber) {
-        this.insuranceCompanyModalityId = insuranceCompanyModalityId;
-        this.insuranceModalityName = insuranceModalityName;
-        this.suggestedModalityIndType = suggestedModalityIndType;
+        this(insuranceCompanyModalityId, insuranceModalityName, suggestedModalityIndType);
         this.publicationOrderNumber = publicationOrderNumber;
         this.setInsuranceModalityType(insuranceModalityType);
+    }
+
+    public InsuranceProductModalityDAO(String insuranceCompanyModalityId, String insuranceModalityName, String insuranceModalityType, String suggestedModalityIndType, String insuranceCompanyId, String insuranceCompanyDesc) {
+        this(insuranceCompanyModalityId, insuranceModalityName, suggestedModalityIndType);
+        this.setInsuranceModalityType(insuranceModalityType);
+        this.insuranceCompanyId = insuranceCompanyId;
+        this.insuranceCompanyDesc = insuranceCompanyDesc;
     }
 
     private String insuranceCompanyModalityId;
     private String insuranceModalityName;
     private String suggestedModalityIndType;
+    private String insuranceCompanyId;
+    private String insuranceCompanyDesc;
     private BigDecimal publicationOrderNumber;
 
     public String getInsuranceCompanyModalityId() { return insuranceCompanyModalityId; }
@@ -34,6 +45,10 @@ public class InsuranceProductModalityDAO extends CommonsDAO {
     public void setInsuranceModalityName(String insuranceModalityName) { this.insuranceModalityName = insuranceModalityName; }
     public String getSuggestedModalityIndType() { return suggestedModalityIndType; }
     public void setSuggestedModalityIndType(String suggestedModalityIndType) { this.suggestedModalityIndType = suggestedModalityIndType; }
+    public String getInsuranceCompanyId() {return insuranceCompanyId;}
+    public void setInsuranceCompanyId(String insuranceCompanyId) {this.insuranceCompanyId = insuranceCompanyId;}
+    public String getInsuranceCompanyDesc() {return insuranceCompanyDesc;}
+    public void setInsuranceCompanyDesc(String insuranceCompanyDesc) {this.insuranceCompanyDesc = insuranceCompanyDesc;}
     public BigDecimal getPublicationOrderNumber() {return publicationOrderNumber;}
     public void setPublicationOrderNumber(BigDecimal publicationOrderNumber) {this.publicationOrderNumber = publicationOrderNumber;}
 
