71e1594825413e5a58800f3409ad7dc3ce36d61a
RONALD FELIPE DOLORES GUARDIA <P030557@PE.igrupobbva>
2021-10-13 10:41:44 -0500

    Adding Rimac object error

---
+++ src/main/java/com/bbva/pisd/dto/insurance/bo/ErrorRimacBO.java
@@ -0,0 +1,14 @@
+package com.bbva.pisd.dto.insurance.bo;
+
+public class ErrorRimacBO {
+
+    private ErrorResponseBO error;
+
+    public ErrorResponseBO getError() {
+        return this.error;
+    }
+    public void setError(ErrorResponseBO error) {
+        this.error = error;
+    }
+
+}
