962246307808ecd4a9b317a81c2a8c25288cc8c0
jose.sandoval.tirado.contractor <jose.sandoval.tirado.contractor@bbva.com>
2022-04-27 01:14:33 -0500

    fix: add dtos for emision gifole service

---
+++ src/main/java/com/bbva/pisd/dto/insurance/aso/gifole/ValidityPeriodASO.java
@@ -0,0 +1,26 @@
+package com.bbva.pisd.dto.insurance.aso.gifole;
+
+public class ValidityPeriodASO {
+    private String startDate;
+    private String endDate;
+
+    public ValidityPeriodASO(String startDate, String endDate) {
+        this.startDate = startDate;
+        this.endDate = endDate;
+    }
+    
+    public String getStartDate() {
+        return startDate;
+    }
+    public void setStartDate(String startDate) {
+        this.startDate = startDate;
+    }
+    public String getEndDate() {
+        return endDate;
+    }
+    public void setEndDate(String endDate) {
+        this.endDate = endDate;
+    }
+
+    
+}
