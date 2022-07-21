454c93df5ea18731e01ebeb4dc5f469d71789135
jose.sandoval.tirado.contractor <jose.sandoval.tirado.contractor@bbva.com>
2022-07-21 04:27:58 -0500

    fix: coverage dto's for gifole home quotation serv

---
+++ src/main/java/com/bbva/pisd/dto/insurance/aso/gifole/UnitASO.java
@@ -0,0 +1,23 @@
+package com.bbva.pisd.dto.insurance.aso.gifole;
+
+import com.bbva.pisd.dto.insurance.aso.CommonFieldsASO;
+
+public class UnitASO extends CommonFieldsASO{
+
+    private String unitType;
+
+    public String getUnitType() {return unitType;}
+    public void setUnitType(String unitType) {this.unitType = unitType;}
+
+    @Override
+    public String toString() {
+        final StringBuilder sb = new StringBuilder("UnitASO{");
+        sb.append("unitType=").append(unitType).append('\'');
+        sb.append("amount=").append(this.getAmount()).append('\'');
+        sb.append("currency=").append(this.getCurrency()).append('\'');
+        sb.append("text=").append(this.getText()).append('\'');
+        sb.append('}');
+        return sb.toString();
+    }
+    
+}
