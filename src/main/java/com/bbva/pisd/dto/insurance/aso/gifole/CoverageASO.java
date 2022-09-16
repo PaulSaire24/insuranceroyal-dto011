7f3c78e1ddb360ed7237733d0035849e47e22061
jose.sandoval.tirado.contractor <jose.sandoval.tirado.contractor@bbva.com>
2022-09-16 14:35:27 -0500

    fix: coverageGroup for aso dto's quotation lead gifole serv Hogar total

--- src/main/java/com/bbva/pisd/dto/insurance/aso/gifole/CoverageASO.java
+++ src/main/java/com/bbva/pisd/dto/insurance/aso/gifole/CoverageASO.java
@@ -7,14 +7,18 @@ import com.bbva.pisd.dto.insurance.aso.CommonFieldsASO;
 public class CoverageASO extends CommonFieldsASO{
 
     private List<CoverageFeeASO> coverageFees;
-
+    private CoverageGroupASO coverageGroup;
+    
     public List<CoverageFeeASO> getCoverageFees() {return coverageFees;}
     public void setCoverageFees(List<CoverageFeeASO> coverageFees) {this.coverageFees = coverageFees;}
-    
+    public CoverageGroupASO getCoverageGroup() {return coverageGroup;}
+    public void setCoverageGroup(CoverageGroupASO coverageGroup) {this.coverageGroup = coverageGroup;}
+
     @Override
     public String toString() {
         final StringBuilder sb = new StringBuilder("CoverageASO{");
         sb.append("coverageFees=").append(coverageFees);
+        sb.append(", coverageGroup=").append(this.getCoverageGroup()).append('\'');
         sb.append(", id=").append(this.getId()).append('\'');
         sb.append(", name='").append(this.getName()).append('\'');
         sb.append('}');
