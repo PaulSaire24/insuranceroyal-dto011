ff4919b70b9810cc032feb222816b0e70cdaae5e
Carlos Carrillo <carlos.carrillo.delgado.contractor@bbva.com>
2022-11-15 17:21:19 -0500

    PERAR-19172 Update params CoberturaBO to responseRimac

--- src/main/java/com/bbva/pisd/dto/insurance/bo/CommonBO.java
+++ src/main/java/com/bbva/pisd/dto/insurance/bo/CommonBO.java
@@ -2,8 +2,6 @@ package com.bbva.pisd.dto.insurance.bo;
 
 public class CommonBO {
 
-
-
     private String id;
 
     private String descripcionCobertura;
@@ -12,15 +10,7 @@ public class CommonBO {
     private Double primaNeta;
     private String moneda;
     private Double sumaAsegurada;
-    private String principal;
-    private String observacionCobertura;
-    private String periodoEvento;
-    private String condicion;
-    private String porcentajeSeleccionado;
-    private int codigoAgrupador;
-    private String tipoAgrupador;
-    private String indSeleccionar;
-    private AgrupadorCoberturaBO agrupador;
+
     private String producto;
 
     public String getId() { return id; }
@@ -46,78 +36,6 @@ public class CommonBO {
         this.descripcionCobertura = descripcionCobertura;
     }
 
-    public String getPrincipal() {
-        return principal;
-    }
-
-    public void setPrincipal(String principal) {
-        this.principal = principal;
-    }
-
-    public String getObservacionCobertura() {
-        return observacionCobertura;
-    }
-
-    public void setObservacionCobertura(String observacionCobertura) {
-        this.observacionCobertura = observacionCobertura;
-    }
-
-    public String getPeriodoEvento() {
-        return periodoEvento;
-    }
-
-    public void setPeriodoEvento(String periodoEvento) {
-        this.periodoEvento = periodoEvento;
-    }
-
-    public String getCondicion() {
-        return condicion;
-    }
-
-    public void setCondicion(String condicion) {
-        this.condicion = condicion;
-    }
-
-    public String getPorcentajeSeleccionado() {
-        return porcentajeSeleccionado;
-    }
-
-    public void setPorcentajeSeleccionado(String porcentajeSeleccionado) {
-        this.porcentajeSeleccionado = porcentajeSeleccionado;
-    }
-
-    public int getCodigoAgrupador() {
-        return codigoAgrupador;
-    }
-
-    public void setCodigoAgrupador(int codigoAgrupador) {
-        this.codigoAgrupador = codigoAgrupador;
-    }
-
-    public String getTipoAgrupador() {
-        return tipoAgrupador;
-    }
-
-    public void setTipoAgrupador(String tipoAgrupador) {
-        this.tipoAgrupador = tipoAgrupador;
-    }
-
-    public String getIndSeleccionar() {
-        return indSeleccionar;
-    }
-
-    public void setIndSeleccionar(String indSeleccionar) {
-        this.indSeleccionar = indSeleccionar;
-    }
-
-    public AgrupadorCoberturaBO getAgrupador() {
-        return agrupador;
-    }
-
-    public void setAgrupador(AgrupadorCoberturaBO agrupador) {
-        this.agrupador = agrupador;
-    }
-
     @Override
     public String toString() {
         final StringBuilder sb = new StringBuilder("CommonBO{");
@@ -128,15 +46,6 @@ public class CommonBO {
         sb.append(", primaNeta=").append(primaNeta);
         sb.append(", moneda='").append(moneda).append('\'');
         sb.append(", sumaAsegurada=").append(sumaAsegurada);
-        sb.append(", principal='").append(principal).append('\'');
-        sb.append(", observacionCobertura='").append(observacionCobertura).append('\'');
-        sb.append(", periodoEvento='").append(periodoEvento).append('\'');
-        sb.append(", condicion='").append(condicion).append('\'');
-        sb.append(", porcentajeSeleccionado='").append(porcentajeSeleccionado).append('\'');
-        sb.append(", codigoAgrupador=").append(codigoAgrupador);
-        sb.append(", tipoAgrupador='").append(tipoAgrupador).append('\'');
-        sb.append(", indSeleccionar='").append(indSeleccionar).append('\'');
-        sb.append(", agrupador=").append(agrupador);
         sb.append(", producto='").append(producto).append('\'');
         sb.append('}');
         return sb.toString();
